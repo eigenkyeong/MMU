b6b6312e3f3435d4beb6cf87d4699e05119cc338
eigenkyeong <76428047+eigenkyeong@users.noreply.github.com>
2023-04-15 19:42:22 +0900

    Update my_trav.c

--- my_trav.c
+++ my_trav.c
@@ -1,7 +1,7 @@
 #define OFFSET_MASK 0x03
 #define PFN_MASK 0xFC
 
-char ku_traverse(void *ku_cr3, char va)
+char my_traverse(void *my_cr3, char va)
 {
 	char *entry;
 	char pa;
@@ -13,7 +13,7 @@ char ku_traverse(void *ku_cr3, char va)
 		return 0; /* NULL pointer */
 
 	/* page table */
-	entry = (char *)ku_cr3 + pte_offset;
+	entry = (char *)my_cr3 + pte_offset;
 	if( !(*entry & 0x01) || (*entry & 0x02) )
 		return 0;
 
